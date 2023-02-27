e17fc5cdf0160d472cd49a9070742dc10ceec522
deepod <davidapitt@gmail.com>
2023-02-27 10:05:56 +0100

    First commit

---
+++ src/TrainTest.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class TrainTest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TrainTest
+{
+    public static void main(String[] args){
+        
+        //Train train1 = new Train("Glasgow", 126,100,50, "Bob", 1234);
+        
+        //System.out.println(train1);
+        
+    }
+}
